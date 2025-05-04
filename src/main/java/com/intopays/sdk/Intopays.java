dd2e387724d2232852f0219a0c6e93cb348f2fbc
Lucas Lopes <lucass.code@gmail.com>
2025-05-04 09:11:31 -0400

    chore: start project

---
+++ src/main/java/com/intopays/sdk/Intopays.java
@@ -0,0 +1,11 @@
+package com.intopays.sdk;
+
+public class Intopays {
+    private final String token;
+    private final String mode;
+
+    public Intopays(String token, String mode) {
+        this.token = token;
+        this.mode = mode;
+    }
+}
